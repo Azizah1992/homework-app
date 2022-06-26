cc0f8d7ee81db56189f713e97e7492b7b18d4fb6
Azizah369 <azizah2134@gmail.com>
2022-06-27 02:20:53 +0300

    first commit

---
+++ src/routes/homeworks.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from 'fastify';
+
+export default async function (server: FastifyInstance) {
+	server.get('/azizah', async (request, reply) => {
+		// add your code here
+	});
+
+	server.get('/Homework/azizah', async (request, reply) => {
+		// add your code here
+	});
+}
